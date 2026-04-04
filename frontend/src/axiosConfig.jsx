358b82962a4ed4ca5af42bf1db8d044777ec8075
Swanwaree <wanwaree.sinthu@gmail.com>
2026-04-04 22:44:56 +1000

    chore: save environment config and dependencies

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.107.57.36:5001', // live
+  baseURL: 'http://15.135.198.207:5001', // local
+  //baseURL: 'http://15.135.198.207:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
